fce968f7a86b17d7d1657d74af01928776b41d08
Morgan Bierbrunner <mbierbrunner@gmail.com>
2025-07-09 18:43:10 -0400

    tooltip added

--- cp_1.js
+++ cp_1.js
@@ -7,8 +7,23 @@ const feedbackDisplay = document.getElementById("feedback-display");
 const formWrapper = document.getElementById("form-wrapper");
 
 //character counter
-comments.addEventListener("input",() =>{
-    charCount.textContent = ${comments.ariaValueMax.length} characters;
-    });
+comments.addEventListener('input', () => {
+  charCount.textContent = `${comments.value.length} characters`;
+});
 
-    
+//tooltips
+document.addEventListener("mouseover", (e) =>{
+    if (e.target.matches("input,texareas")){
+        const tooltip = document.createElement("div");
+        tooltip.className ="tooltip";
+        tooltip.textContent =e.target.dataset.tooltip;
+        document.body.appendChild(tooltip);
+        const rect = e.target.getBoundingClientRect();
+        tooltip.style.left = `${rect.left}px`;
+        tooltip.style.top = `${rect.bottom+5}px`;
+        tooltip.style.display ="block";
+        e.target.tooltipEl = tooltip;
+    }
+    });
+}
+)
